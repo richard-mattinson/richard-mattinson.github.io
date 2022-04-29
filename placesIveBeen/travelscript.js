301fe02e5ffbc16b33c2110ef38c37537e5e863e
Richard Mattinson <richard.m@ttinson.net>
2022-04-29 15:58:37 +0100

    Added empty links for Read More, and live links for Github Repos

---
+++ placesIveBeen/travelscript.js
@@ -0,0 +1,10 @@
+/* Comments */
+// function placesHover(){
+//     document.querySelector('#places').style.color="orange"
+//     }    
+//     function placesOut(){
+//     document.querySelector('#places').style.color="white"
+//     }
+function showAlert(){
+alert("This is absolutely going to go somewhere someday...")
+}
